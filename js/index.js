1c83f56852bdbb3c6da93e8819a9edd3fdce41b9
chloehan <hanj951122@gmail.com>
2024-06-26 14:45:10 +0900

    update_Wed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // $("body").addClass("hidden");
+
     // // 연령제한 팝업[테스트]
     // $("#enter").click(function(event){
     //     event.preventDefault(); // 폼 제출 막기!!!
@@ -10,6 +12,7 @@ $(document).ready(function(){
     // 연령제한 팝업
     $('#enter').click(function(e){
         e.preventDefault();
+
         // 값 정수로 변환
         let year = parseInt($('#user_year').val());
         let month = parseInt($('#user_month').val()) - 1; // JavaScript의 월은 0부터 시작
@@ -35,8 +38,10 @@ $(document).ready(function(){
         if (age >= 19) {
             $('.modal_back').addClass('hidden');
             $('.popup_age').addClass('hidden');
+            $("body").removeClass("hidden");
         } else {
             alert('만 19세 이상만 이용이 가능합니다.');
+            // $("body").addClass("hidden");
         }
     });
 
@@ -44,14 +49,18 @@ $(document).ready(function(){
     $(".gnb_btn").click(function(){
         $(".modal_gnb").addClass("on")
         $(".gnb_mo").addClass("on")
+        $("body").addClass("hidden")
     })
     $(".gnb_btn_close").click(function(){
         $(".modal_gnb").removeClass("on")
         $(".gnb_mo").removeClass("on")
+        $("body").removeClass("hidden")
     })
 
     let winWidth = window.innerWidth;
-    if(winWidth > 500) {
+    let intervalId;
+
+    if(winWidth > 768) {
         // 탭버튼 + 슬라이드 => 두개 연결
         let count = 0;
         $(".ul_title>li").click(function(e){
@@ -62,30 +71,26 @@ $(document).ready(function(){
             $(".ul_train").css("transform",`translateX(calc(${-33.333*count}% - 50px))`)
         })
 
-        setInterval(function(){
+        intervalId = setInterval(function(){
             count++
             if(count>2){count=0}
             $(".ul_title>li").removeClass("on")
             $(".ul_title>li").eq(count).addClass("on")
             $(".ul_train").css("transform",`translateX(calc(${-33.333*count}% - 50px))`)
         },5000)
-    } else {
+    } else{
         let count = 0;
         $(".ul_title>li").click(function(e){
             e.preventDefault()
             $(".ul_title>li").removeClass("on")
             $(this).addClass("on")
             count = $(this).index()
-            $(".ul_train").css("transform",`translateX(calc(${-33.333*count}%))`)
+            $(".ul_train").css("transform",`translateX(calc(${-33.333*count}%))`).css("transition", "none")
         })
-
-        setInterval(function(){
-            count++
-            if(count>2){count=0}
-            $(".ul_title>li").removeClass("on")
-            $(".ul_title>li").eq(count).addClass("on")
-            $(".ul_train").css("transform",`translateX(calc(${-33.333*count}%))`)
-        },8000)
+        
+        if (intervalId) {
+            clearInterval(intervalId); // setInterval 제거
+        }
     }
 
     // a버튼 기능 무력화
